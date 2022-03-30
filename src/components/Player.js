0330c09c0a0fdec32c34dc838ebbb3c711eeb234
DarifElmehdi <elmehdi.darif@um5s.net.ma>
2022-03-30 13:23:29 +0000

    add play and pause button

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import { PlayIcon, PauseIcon } from "@heroicons/react/outline";
 function Player(props) {
     let { number_of_surah } = props;
     function padLeadingZeros(num, size) {
@@ -8,33 +8,59 @@ function Player(props) {
         return s;
     }
     let [url, setUrl] = useState("");
+    let [play, setPlay] = useState(false);
     function reloadPlayer() {
         if (number_of_surah) {
+            setUrl(
+                "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/" +
+                    padLeadingZeros(number_of_surah, 3) +
+                    ".mp3"
+            );
             document.getElementById("my-audio").pause();
             document.getElementById("my-audio").load();
             document.getElementById("my-audio").play();
+            setPlay(true);
         }
     }
+
+    function playQuran() {
+        document.getElementById("my-audio").play();
+        setPlay(true);
+    }
+
+    function pauseQuran() {
+        document.getElementById("my-audio").pause();
+        setPlay(false);
+    }
     useEffect(() => {
-        setUrl(
-            "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/" +
-                padLeadingZeros(number_of_surah, 3) +
-                ".mp3"
-        );
         reloadPlayer();
         return (number_of_surah = 0);
     }, [number_of_surah]);
     return (
         <>
             {number_of_surah === 0 ? null : (
-                <audio
-                    id="my-audio"
-                    controls
-                    autoPlay
-                    className="fixed bottom-0 w-full border-2 border-cyan-600 rounded-full"
-                >
-                    <source src={url} />
-                </audio>
+                <div className="fixed bottom-4 right-4 flex justify-between bg-white rounded-full">
+                    <audio
+                        id="my-audio"
+                        controls
+                        autoPlay
+                        className="hidden border-2 border-cyan-600 rounded-full"
+                    >
+                        <source src={url} />
+                    </audio>
+                    {!play && (
+                        <PlayIcon
+                            className="w-12 h-12 bg-cyan-500 rounded-full cursor-pointer"
+                            onClick={playQuran}
+                        />
+                    )}
+                    {play && (
+                        <PauseIcon
+                            className="w-12 h-12 bg-cyan-500 rounded-full cursor-pointer"
+                            onClick={pauseQuran}
+                        />
+                    )}
+                </div>
             )}
         </>
     );
