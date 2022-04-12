fa622c094e5906acd9288cbb376b09fa6010ddfc
DarifElmehdi <elmehdi.darif@um5s.net.ma>
2022-04-12 20:10:05 +0000

    fix build issue Module not found!

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,18 @@
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import React from "react";
+function Player(props) {
+    let { audio, id } = props;
+    return (
+        <div className="fixed w-full bottom-0">
+            <AudioPlayer
+                volume={0.5}
+                autoPlay
+                id={id}
+                src={audio}
+                onPlay={(e) => console.log("onPlay")}
+            />
+        </div>
+    );
+}
+export default Player;
