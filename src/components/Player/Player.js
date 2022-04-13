93f2914e26acca7d27261fd43513e0f723ac8d48
DarifElmehdi <elmehdi.darif@um5s.net.ma>
2022-04-13 19:32:29 +0000

    undefined! bug fix

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -16,23 +16,29 @@ function Player(props) {
 
     return (
         <div className="fixed w-full bottom-0">
-            <div className="h-8 w-full bg-white flex justify-evenly items-start">
-                <h1>
-                    {
-                        audiolist[
-                            audiolist.findIndex((item) => item.url === src) + 1
-                        ].en_name
-                    }
-                </h1>
-                <h1>: </h1>
-                <h1 dir="rtl">
-                    {
-                        audiolist[
-                            audiolist.findIndex((item) => item.url === src) + 1
-                        ].ar_name
-                    }
-                </h1>
-            </div>
+            {audiolist.length > 0 && (
+                <div className="h-8 w-full bg-white flex justify-evenly items-start">
+                    <h1>
+                        {
+                            audiolist[
+                                audiolist.findIndex(
+                                    (item) => item.url === src
+                                ) + 1
+                            ].en_name
+                        }
+                    </h1>
+                    <h1>: </h1>
+                    <h1 dir="rtl">
+                        {
+                            audiolist[
+                                audiolist.findIndex(
+                                    (item) => item.url === src
+                                ) + 1
+                            ].ar_name
+                        }
+                    </h1>
+                </div>
+            )}
             <AudioPlayer
                 id="player"
                 volume={0.5}
