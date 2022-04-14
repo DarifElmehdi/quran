9477e313c6031be54f63150538e69378841f771f
DarifElmehdi <elmehdi.darif@um5s.net.ma>
2022-04-14 13:23:19 +0000

    Floating search barre on mobile devices

--- src/components/pages/HeroSection.js
+++ src/components/pages/HeroSection.js
@@ -3,10 +3,10 @@ import React from "react";
 function HeroSection(props) {
     let { img } = props;
     return (
-        <div className="bg-hero-bg md:bg-cover bg-center bg-no-repeat w-full flex justify-center py-20">
+        <div className="h-96 bg-mobile-hero-bg md:bg-hero-bg md:bg-cover bg-center bg-no-repeat w-full flex justify-center py-20">
             <div className="relative z-0">
-                <div className=" flex justify-center my-8 md:my-12">
-                    <img src={img} alt={img} className="h-36 md:h-52 w-auto" />
+                <div className=" flex justify-center my-8 ">
+                    <img src={img} alt={img} className="h-36 w-auto" />
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
     theme: {
         extend: {
             backgroundImage: {
-                "hero-bg": "url('../public/assets/hero-bg.avif')",
+                "hero-bg": "url('../public/assets/hero-bg.jpg')",
+                "mobile-hero-bg": "url('../public/assets/mobile-hero-bg.jpg')",
             },
         },
     },
